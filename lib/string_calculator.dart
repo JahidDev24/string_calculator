6a1c2b872e5191baf7fa21bc95ebcbd710591438
JahidDev24 <developer.jahidkhan@gmail.com>
2026-02-06 00:43:50 +0530

    feat: parse custom delimiter

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,9 +1,20 @@
 class StringCalculator {
 int add(String numbers) {
   if (numbers.isEmpty) return 0;
- final mynum =  numbers.replaceAll("\n",",");
-  final parts = mynum.split(',');
+
+  String delimiter = ',';
+  String values = numbers;
+
+  if (numbers.startsWith('//')) {
+    delimiter = numbers[2];
+    values = numbers.substring(4);
+  }
+
+  values = values.replaceAll('\n', delimiter);
+  final parts = values.split(delimiter);
+
   return parts.map(int.parse).reduce((a, b) => a + b);
 }
+
 }
 
