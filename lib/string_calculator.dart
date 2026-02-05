2848ee36493456a9bbb3fbfae2a032423bf52f66
JahidDev24 <developer.jahidkhan@gmail.com>
2026-02-06 00:34:35 +0530

    feat: support single number

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,7 +1,7 @@
 class StringCalculator {
-  int add(String numbers) {
-    if (numbers.isEmpty) return 0;
-    return 0;
-  }
+int add(String numbers) {
+  if (numbers.isEmpty) return 0;
+  return int.parse(numbers);
+}
 }
 
